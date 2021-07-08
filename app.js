78e8a0a3756ab0e9fecfbf05d137d9d3fbe27f8c
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-09 01:32:24 +0800

    Add delete restaurant function

--- app.js
+++ app.js
@@ -99,6 +99,13 @@ app.post('/restaurants/:restaurant_id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`This server is running on localhost:${port}`)

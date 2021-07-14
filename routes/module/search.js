368e95c51e2f8f9707d9e80bc03742fa5a3719e1
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-15 02:56:45 +0800

    Add sort function

--- routes/module/search.js
+++ routes/module/search.js
@@ -3,7 +3,20 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  const keyword = req.query.keyword
+  const { keyword, sortItem, sortMethod } = req.query
+  const sort = {}
+  sort[sortItem] = sortMethod
+
+  if (!!sortItem) {
+    Restaurant.find()
+      .lean()
+      .sort(sort)
+      .then(restaurantList => {
+        const restaurants = restaurantList.filter((restaurant) => restaurant.name.toLowerCase().includes(keyword.toLowerCase().trim()))
+        res.render('index', { restaurants, keyword, sortItem, sortMethod })
+      })
+      .catch(error => console.log(error))
+  } else {
   Restaurant.find()
     .lean()
     .then(restaurantList => {
@@ -11,6 +24,7 @@ router.get('/', (req, res) => {
       res.render('index', { restaurants, keyword })
     })
     .catch(error => console.log(error))
+  }
 })
 
 module.exports = router

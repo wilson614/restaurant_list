188cd71b74379bab09d1b25e0a8ec85b09e98631
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-01 23:28:26 +0800

    Add user authentication

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const home = require('./module/home')
 const restaurants = require('./module/restaurants')
 const search = require('./module/search')
 const users = require('./module/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/restaurants', restaurants)
-router.use('/search', search)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/search', authenticator, search)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router

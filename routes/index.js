8a94392cf7cf553bf984e65d8b4fcfd27d79a80e
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-01 20:27:02 +0800

    Add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./module/home')
 const restaurants = require('./module/restaurants')
 const search = require('./module/search')
+const users = require('./module/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
 
 module.exports = router

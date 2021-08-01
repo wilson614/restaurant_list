e085bd9fafa95844aedfac012507536a36b8cd84
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-02 00:04:30 +0800

    Add isAuthenticated to display different templates

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.use(session({
   saveUninitialized: true
 }))
 usePassport(app)
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {

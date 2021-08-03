b9a375ce64c17f95fd4d7bbfaa8c4271d2096b29
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-04 02:18:14 +0800

    Modify seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,44 @@
+/* eslint-disable camelcase */
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
+const User = require('../user')
+
+const defaultUsers = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(0, 4)
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(4, 8)
+  }
+]
 
 db.once('open', () => {
-  restaurantList.results.forEach(restaurant =>
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
-    }))
-  console.log('done!')
+  Promise.all(Array.from(defaultUsers, (defaultUser) => {
+    const { name, email, password, restaurants } = defaultUser
+    return bcrypt.genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({ name, email, password: hash }))
+      .then(user => {
+        return Promise.all(Array.from(restaurants, (restaurant) => {
+          const { name, name_en, category, image, location, phone, google_map, rating, description } = restaurant
+          const userId = user._id
+          return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
+        }))
+      })
+  }))
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
 })
